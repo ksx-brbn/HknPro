addcfa98a7e5916cd718430aea4663327ca92e75
KASIX Kokubo <bs-kokubo@kasix.co.jp>
2025-08-19 17:06:26 +0900

    feat:計上分未払費用支払実績 追加

--- BourbonWeb/Controllers/SamplesController.cs
+++ BourbonWeb/Controllers/SamplesController.cs
@@ -622,6 +622,11 @@ FROM
             return View();
         }
 
+        public IActionResult RAR0080Sample()
+        {
+            return View();
+        }
+
         public IActionResult RAV0050Sample()
         {
             return View();
