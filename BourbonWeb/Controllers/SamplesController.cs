680aebc4c6656cb59eb34e8138b1966a1dbfc46a
KASIX Kokubo <bs-kokubo@kasix.co.jp>
2025-08-19 14:53:56 +0900

    feat:鑑添付情報履歴削除 追加

--- BourbonWeb/Controllers/SamplesController.cs
+++ BourbonWeb/Controllers/SamplesController.cs
@@ -576,6 +576,11 @@ FROM
         {
             return View();
         }
+
+        public IActionResult CMB0020Sample()
+        {
+            return View();
+        }
         #endregion
     }
 }
